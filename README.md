db486889b9dc33ed58ea010cafd3f7da5d7f6d66
JAZIEL LOPEZ <jaziel.lopez@thermofisher.com>
2019-07-16 14:40:11 -0700

    initial import 1.0.0.

---
+++ README.md
@@ -0,0 +1,44 @@
+## TEMPLATE VELOCITY VIEWER
+--
+
+Parse Velocity template files from command line and view it in your browser
+
+### INSTALLATION
+
+```
+$ npm install 
+```
+
+### USAGE
+
+```
+node render-velocity-template.js --help
+Usage: render-velocity-template.js --template|-t [string] --data|-d [string]
+
+Options:
+  --version       Show version number                                  [boolean]
+  --template, -t  path to velocity template                             [string]
+  --data, -d      key=value template data.
+                    --data "foo=bar&baz=one"                              [string]
+  --help, -h      Show help
+```
+
+### EXAMPLE 
+
+In order to parse and display a velocity template file you need to provide the following information:
+
+1. Provide path to template file
+2. Provide data for template placeholders in file
+   Data should be provided in the form of key=value. If you want to concatenate more than one key-value use `&` separator.
+
+```
+$ node render-velocity-template.js --template /path/to/file.vm --data "foo=bar&bar=baz"
+```
+
+### VERSION
+
+1.0.0 Initial
+
+### CONTACT
+
+Jaziel Lopez <jaziel.lopez @ thermofisher.com>
---
+++ render-velocity-template.js
@@ -0,0 +1,102 @@
+"use strict";
+
+/**
+ * Velocity Custom Viewer
+ * ================================================================
+ * usage:
+ *    node src/index.js --template /path/template.vm --data "foo=bar&bar=baz"
+ *
+ * author:
+ *    jaziel.lopez @ thermofisher.com
+ *
+ **/
+
+const fs = require('fs'),
+    v = require('velocityjs'),
+    y = require('yargs'),
+    opn = require('opn'),
+    argv = y
+        .usage('Usage: $0 --template|-t [string] --data|-d [string]')
+        .option("template",
+            {
+                alias: "t",
+
+                description: "path to velocity template",
+
+                type: "string"
+                }
+            )
+        .option("data",
+            {
+                alias: "d",
+
+                description: "key=value template data.\n--data \"foo=bar&baz=one\"",
+
+                type: "string"
+                }
+            )
+        .help().alias("help", "h").argv;
+
+const tmpl = argv.template || "",
+
+    data = argv.data || "",
+
+    nodes = data.split("&");
+
+/**
+ *  Parse template data
+ */
+const parseTemplateData = function(nodes) {
+
+    let templateInfo = {};
+
+    nodes.forEach(function(i) {
+
+       let kv = i.split("=");
+
+       templateInfo[kv[0]] = kv[1];
+
+    });
+
+    return templateInfo;
+
+};
+
+/**
+ * Read template file
+ */
+const readTemplateFile = function(tmpl) {
+
+    return fs.readFileSync(tmpl, "utf8");
+
+};
+
+/**
+ * Render velocity template upon given arguments
+ */
+const doRender = function() {
+
+    const _f = readTemplateFile(tmpl),
+
+        _d = parseTemplateData(nodes),
+
+        render = v.render(_f, _d),
+
+        vmpath = "/tmp/vm.html";
+
+    fs.writeFile(vmpath, render, function(error) {
+
+        if(error) {
+            throw error;
+        }
+
+        opn("file://" + vmpath);
+
+       console.log("velocity template render complete!");
+
+    });
+};
+
+
+
+doRender();
